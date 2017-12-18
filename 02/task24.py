cb8b0ec45eb2e3b1375105b2697e9b4a35f86907
Omar Gutiérrez <omar.vpa@gmail.com>
2017-12-18 21:47:46 +0100

    First version of 2.4 completed

--- 02/task24.py
+++ 02/task24.py
@@ -6,6 +6,23 @@ Task 2.4 Boolean functions and the Boolean Fourier transform
 import numpy as np
 import numpy.linalg as la
 import matplotlib.pyplot as plt
+from itertools import chain, combinations
+from functools import reduce
+
+
+def powerset(s):
+    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+
+
+def phi(x):
+    """
+    Second part of the task
+    """
+    length = len(x)
+    ps = powerset(np.arange(length))
+    phi = map(lambda e: reduce(lambda xe, ye: xe * x[ye], e, 1), list(ps))
+    return list(phi)
 
 
 def data_matrix_V1(x):
@@ -16,11 +33,6 @@ def data_matrix_V1(x):
 
 def lsq_solution_V1(X, y):
     w = np.dot(np.dot(la.inv(np.dot(X.T, X)), X.T), y)
-    return w
-
-
-def lsq_solution_V2(X, y):
-    w = np.dot(la.pinv(X), y)
     return w
 
 
@@ -39,7 +51,7 @@ def wolfram_rule(number):
     #wolfram_number = wolfram_number.reshape(shape)
     return wolfram_number
 
-
+# First part of the task
 # design matrix X, tricky way ;)
 integers = np.linspace(7, 0, 8).astype(np.uint8)
 bits = np.unpackbits(integers)
@@ -52,21 +64,48 @@ y_126 = wolfram_rule(126)
 
 # y_hat matrix
 X = data_matrix_V1(bits)
-w_110 = lsq_solution_V2(X, y_110)
-w_126 = lsq_solution_V2(X, y_126)
+w_110 = lsq_solution_V3(X, y_110)
+w_126 = lsq_solution_V3(X, y_126)
 
-print(w_110)
-print(w_126)
+#print(w_110)
+#print(w_126)
 
 yhat_110 = np.dot(X, w_110)
 yhat_126 = np.dot(X, w_126)
 
 print("Comparison between yhat and y. 126")
 print(y_126)
-print(yhat_126)
 print(np.around(yhat_126))
 
 print("Comparison between yhat and y. 110")
 print(y_110)
-print(yhat_110)
 print(np.around(yhat_110))
+
+# Third part of the task
+# Generate the PHI matrix
+big_phi = phi(bits[0])
+big_phi = np.array(big_phi)
+big_phi = big_phi.reshape(8, 1)
+
+for b in bits[1:]:
+    bi = phi(b)
+    bi = np.array(bi)
+    bi = bi.reshape(8, 1)
+    print(big_phi.shape)
+    print(bi.shape)
+    big_phi = np.hstack((big_phi, bi))
+
+
+print("------------------------------------")
+print("Comparison between yhat and y. 110")
+w_110 = lsq_solution_V3(big_phi, y_110)
+yhat_110 = np.dot(big_phi, w_110)
+print(y_110)
+print(np.around(yhat_110))
+
+print("Comparison between yhat and y. 126")
+w_110 = lsq_solution_V3(big_phi, y_110)
+w_126 = lsq_solution_V3(big_phi, y_126)
+yhat_126 = np.dot(big_phi, w_126)
+print(y_126)
+print(np.around(yhat_126))
